b2e7391991f84cbdd1955071774b876d800d94da
emobeko <119516877+emobeko@users.noreply.github.com>
2023-12-26 18:05:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,37 @@
 from flask import Flask, request
 import json, inspect
-import telebot
-from binance.spot import Spot as Client
+from binance.um_futures import UMFutures as Client
 
 app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 def webhook():
-    # linke gelen post isteklerini parçala ve diğer fonksiyonlara dağıt.
+    
     try:
         data = json.loads(request.data)
         ticker = data['ticker']
         exchange = data['exchange']
         price = data['price']
         side = data['side']
+        positionSide = data['positionSide']
         quantity = data['quantity']
-        telegramBotApi = data['telegramBotApi']
-        telegramUserId = data['telegramUserId']
         binanceApiKey = data['binanceApiKey']
         binanceSecretKey = data['binanceSecretKey']
         params = {
             "symbol": ticker,
             "side": side,
+            "positionSide": positionSide,
             "type": "MARKET",
             "quantity": quantity,
         }
-        binanceOrder(binanceApiKey, binanceSecretKey, params)
-        telegramSender(telegramBotApi,telegramUserId,ticker,side,exchange,quantity)
+        Client(binanceApiKey, binanceSecretKey).new_order(**params)
+        
     except Exception as e:
         funcName = inspect.stack()[0][3]
         print(f"{funcName}() fonsiyonunda hata oluştu: {e}")
     return {
         "code": "success"
     }
-
-@app.route("/istek", methods=['POST'])
-def tele():
-    return json.loads(request.data)
-
-def binanceOrder(apiKey : str, secretKey : str, params):
-    try:
-        Client(apiKey,secretKey).new_order(**params)
-    except Exception as e:
-        funcName = inspect.stack()[0][3]
-        print(f"{funcName}() fonsiyonunda hata oluştu: {e}")
-
-
-def telegramSender(botApi : str, telegramUserId : str, ticker : str, side : str, exchange : str, quantity : str):
-    try:
-        telebot.TeleBot(botApi).send_message(telegramUserId, f"{ticker} {side}ING on {exchange} \nQuantity : {quantity} ")
-    except Exception as e:
-        funcName = inspect.stack()[0][3]
-        print(f"{funcName}() fonsiyonunda hata oluştu: {e}")
         
         
 
